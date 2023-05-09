e8670697725785c5c4275f48c112a0139a7e7fb8
ViniHipolito <97119997+ViniHipolito@users.noreply.github.com>
2023-05-09 13:12:30 -0300

    bora toma uma

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 var firebaseConfig = {
-
+  apiKey: "AIzaSyBUnzq-kB2kawlGcatLG7W0fgRsAAP62uU",
+  authDomain: "bicicleta-89382.firebaseapp.com",
+  projectId: "bicicleta-89382",
+  storageBucket: "bicicleta-89382.appspot.com",
+  messagingSenderId: "777261825155",
+  appId: "1:777261825155:web:35d0493040fc9b6c652fb6"
 };
 
 firebase.initializeApp(firebaseConfig);
